a5f439822f308162e5e2c80a61fbed5e03d9495f
Sheikh Humayun Roshid <70696397+Humayun-23@users.noreply.github.com>
2024-08-16 22:46:04 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -1,1 +1,55 @@
-#first line in the backup project 
+#!/bin/bash
+#Project started
+if [[ $# != 2 ]]
+then
+  echo "backup.sh target_directory_name destination_directory_name"
+  exit
+fi
+
+
+if [[ ! -d $1 ]] || [[ ! -d $2 ]]
+then
+  echo "Invalid directory path provided"
+  exit
+fi
+
+
+targetDirectory=$1
+destinationDirectory=$2
+
+
+echo "Target Directory: $targetDirectory"
+echo "Destination Directory : $destinationDirectory"
+
+
+currentTS=`date +%s`
+
+backupFileName="backup-[$currentTS].tar.gz"
+
+origAbsPath=`pwd`
+
+
+cd $destinationDirectory
+destDirAbsPath=`pwd`
+
+
+cd $origAbsPath
+cd $targetDirectory
+
+yesterdayTS=$(($currentTS - 24 * 60 * 60))
+
+declare -a toBackup
+
+for file in $(ls)
+do
+  if ((`date -r $file +%s` > $yesterdayTS))
+  then
+   
+toBackup+=($file)
+  fi
+done
+
+
+tar -czvf $backupFileName ${toBackup[@]}
+
+mv $backupFileName $destAbsPath
